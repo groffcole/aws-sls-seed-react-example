a31f1f4645b073b0a5fff9930b5fa12871bd8163
Cole Groff <groffcole@gmail.com>
2020-04-08 23:25:13 -0500

    add prefix to npm commands

---
+++ user_interface/build/precache-manifest.0c802196db0c10075740b11a321f439a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b156412d9d574f95db4062b32136b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f163001ed2f9538c351",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f175fe1409232f430a3d",
+    "url": "/static/js/2.c20cab3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20cab3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f163001ed2f9538c351",
+    "url": "/static/js/main.5f843ede.chunk.js"
+  },
+  {
+    "revision": "8a09f0563cc98204d52f",
+    "url": "/static/js/runtime-main.cabcdb91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
